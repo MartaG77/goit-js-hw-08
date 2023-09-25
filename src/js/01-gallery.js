2c53db2fad44a6b31d463befde0ee008151b53df
MartaG77 <129276573+MartaG77@users.noreply.github.com>
2023-09-25 12:55:28 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,19 +1,14 @@
-// Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
-const gallery = document.querySelector('.gallery');
-
+const imagesGallery = document.querySelector('.gallery');
 galleryItems.forEach(function (item, index) {
-  gallery.insertAdjacentHTML(
-    'beforeend',
-    `<li class="gallery__item"><a class='gallery__link' href='${item.original}'><img src='${item.preview}' alt='${item.description}' class='gallery__image'/></a></li>`
-  );
+  imagesGallery.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery__item"><a class='gallery__link' href='${item.original}'><img src='${item.preview}' alt='${item.description}' class='gallery__image'/></a></li>`)
 });
-const imageModal = new SimpleLightbox('.gallery__link', {
+const imageOpen = new SimpleLightbox('.gallery__link', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-gallery.style.listStyleType = 'none';
 console.log(galleryItems);
